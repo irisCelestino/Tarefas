97d6495559ff7ef1b88b6b78e48899d8a38a1481
irisCelestino <iris_celestino@hotmail.com>
2023-08-28 17:15:33 -0300

    primeiro commit to do list php

---
+++ README.md
@@ -0,0 +1,2 @@
+1 . baixe e instale o xampp
+2 . no disco local :/C salve o arquivo dentro da pasta do xampp/htdocs
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>To do list</title>
+</head>
+
+<body>
+  <div class="main">
+   <div class= "add-sessao">
+     <form action="" method="post">
+     <input 
+     id ="atividade"
+     type="text"  
+     name="titulo" 
+     placeholder="Adicione uma atividade..." 
+     >
+   <button type="submit" id="btAdicionar">Adicionar &nbsp; </button>
+
+
+   </div>
+  
+
+  </form>
+   </div>
+
+  <div class="show-todo-section">
+    <div class = "todo-item">
+      <input type="checkbox">
+      <h2>isso aqui</h2>
+      <br>
+      <small>criado: 28/08/3023</small>
+    </div>
+
+  </div>
+</body>
+
+</html>
