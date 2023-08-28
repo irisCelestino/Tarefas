c7fbc81538f2b9be5926c2f1982e892adfaa4010
irisCelestino <iris_celestino@hotmail.com>
2023-08-28 20:08:09 -0300

    feat: finalização da função add

--- app/add.php
+++ app/add.php
@@ -5,11 +5,25 @@ if (isset($_POST['titulo'])){
 
   $titulo = $_POST['titulo'];
   
-  
-  
   if (empty($titulo)){
     header("Location:../index.php?mess=error");
+  }else{
+    $stmt = $conexao -> prepare("INSERT INTO todos(titulo) VALUE(?)");
+    $res = $stmt-> execute([$titulo]);
+
+    if($res){
+      header("Location: ../index.php?mess=sucess");
+    }else{
+      header ("Location: ../index.php");
+    }
+
+    $conexao= null;
+
+    exit();
+
   }
+}else{
+  header("Location:../index.php?mess=error");
 }
 
 
